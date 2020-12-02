6b39819b8c0be6043018def958aa25ba299f857c
Jeffrey Platt <jsplatt@gmail.com>
2020-12-02 15:20:23 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -8,11 +8,13 @@ def base_hash
 end
 
 def monopoly_with_second_tier
-  monopoly = {
-  :railroads=> {},
-     :pieces => 4,
-     :rent_in_dollars => {},
-     :names => {}
+  monopoly =  {
+  :railroads=>
+  {
+    :pieces=>4,
+    :rent_in_dollars=>
+    {
+    :names  
   
 }
 
