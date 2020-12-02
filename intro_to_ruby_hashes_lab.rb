eb3588e8c82748316c0f10c275cad1be93cbf17d
Jeffrey Platt <jsplatt@gmail.com>
2020-12-02 15:39:11 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -39,9 +39,18 @@ def monopoly_with_third_tier
       {
         "mortgage_value=>100"
       },
-      :pennsylvania_railroad=>{},
-      :b_and_o_railroad=>{},
-      :shortline_railroad=>{}
+      :pennsylvania_railroad=>
+      {
+        "mortgage_value=>200"
+      },
+      :b_and_o_railroad=>
+      {
+        "mortgage_value=>400"
+      },
+      :shortline_railroad=>
+      {
+        "mortgage_value=>800"
+      }
     }
   }
 }
